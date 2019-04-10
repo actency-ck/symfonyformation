da032a04dbe1bbce93886098ac39f9190a6061a7
cklein <christophe.klein@actency.fr>
2019-04-10 14:30:02 +0200

    Comments and type hint

--- src/Services/SayHello.php
+++ src/Services/SayHello.php
@@ -8,15 +8,26 @@ class SayHello {
 
   private $translator;
 
+  /**
+   * SayHello constructor.
+   *
+   * @param \Symfony\Contracts\Translation\TranslatorInterface $translator
+   */
   public function __construct(TranslatorInterface $translator) {
     $this->translator = $translator;
   }
 
-  public function sayHello() {
+  /**
+   * @return string
+   */
+  public function sayHello(): string {
     return 'Hello';
   }
 
-  public function saySfIsGreat() {
+  /**
+   * @return string
+   */
+  public function saySfIsGreat(): string {
     return $this->translator->trans('symfony_is_great');
   }
 
